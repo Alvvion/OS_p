3b6a159a3f8eabbf2429d7db5db2ed2f9df5a571
Utkarsh <alvvion@gmail.com>
2023-12-19 18:31:14 +0530

    ProcessLoader renamed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import WindowManager from "@/components/system/WindowManager";
+import ProcessLoader from "@/components/system/ProcessLoader";
 import { ProcessProvider } from "@/contexts/process";
 
 export default function Home() {
   return (
     <ProcessProvider>
-      <WindowManager />
+      <ProcessLoader />
     </ProcessProvider>
   );
 }
