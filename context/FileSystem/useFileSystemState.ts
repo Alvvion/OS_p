800ad492120fa8e63b2c61af65c4cc7347fbc3e1
Utkarsh <alvvion@gmail.com>
2024-06-21 09:43:20 +0530

    Browserfs is now a lib

--- context/FileSystem/useFileSystemState.ts
+++ context/FileSystem/useFileSystemState.ts
@@ -1,9 +1,13 @@
-import { BFSRequire, configure } from "browserfs";
+import type * as IBrowserFS from "browserfs";
 import type { FSModule } from "browserfs/dist/node/core/FS";
 import { useEffect, useState } from "react";
 
+import * as BrowserFS from "@/public/libs/browserfs/browserfs.min.js";
+
 import FileSystemConfig from "./config";
 import type { FileSystemStateType } from "./types";
+
+const { BFSRequire, configure } = BrowserFS as typeof IBrowserFS;
 
 const useFileSystemState = (): FileSystemStateType => {
   const [fs, setFs] = useState<FSModule>();
