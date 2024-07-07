4bf7b858c3600760e69da97bed3060ad1a962e41
Utkarsh <alvvion@gmail.com>
2024-07-07 12:01:42 +0530

    Moved updating of dragged folder to drop handler

--- components/system/FileManager/useFolder.ts
+++ components/system/FileManager/useFolder.ts
@@ -16,8 +16,7 @@ const useFolder = (
 ): Folder => {
   const { addFile, addFsWatcher, fs, removeFsWatcher, updateFolder } =
     useFileSystem();
-  const { focusEntry,  } =
-    useSession();
+  const { focusEntry } = useSession();
   const [files, setFiles] = useState<string[]>([]);
   const [downloadLink, setDownloadLink] = useState("");
 
@@ -89,10 +88,15 @@ const useFolder = (
     rename = false,
     iteration = 0,
   ): void => {
-    if (!buffer && ![".", directory].includes(dirname(name))) {
-      fs?.rename(name, join(directory, basename(name)), () =>
-        updateFolder(dirname(name), "", name),
-      );
+    if (!buffer && dirname(name) !== ".") {
+      const renamedPath = join(directory, basename(name));
+
+      if (name !== renamedPath) {
+        fs?.rename(name, renamedPath, () => {
+          updateFolder(directory, name);
+          updateFolder(dirname(name), "", name);
+        });
+      }
     } else {
       const uniqueName = iteration ? iterateFileNames(name, iteration) : name;
       const resolvedPath = join(directory, uniqueName);
