96daefac3ddb9ba91910487244815ee99426cd30
Utkarsh <alvvion@gmail.com>
2024-07-12 18:55:22 +0530

    Clean up Monaco

--- components/apps/MonacoEditor/index.tsx
+++ components/apps/MonacoEditor/index.tsx
@@ -1,4 +1,5 @@
 import Editor, { loader } from "@monaco-editor/react";
+import type * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
 import { basename } from "path";
 import { useEffect, useState } from "react";
 
@@ -7,8 +8,11 @@ import { useFileSystem } from "@/context/FileSystem";
 import { useProcesses } from "@/context/Process";
 import { useTheme } from "@/context/Theme";
 import useTitle from "@/hooks/useTitle";
+import { cleanUpGlobals } from "@/utils/functions";
 
 import { overrideSubMenuStyling } from "./functions";
+
+type IStandaloneCodeEditor = Monaco.editor.IStandaloneCodeEditor;
 
 const MonacoEditor: React.FC<ComponentProps> = ({ id }) => {
   const {
@@ -19,6 +23,7 @@ const MonacoEditor: React.FC<ComponentProps> = ({ id }) => {
   const { sizes } = useTheme();
 
   const [value, setValue] = useState("");
+  const [editor, setEditor] = useState<IStandaloneCodeEditor>();
 
   useEffect(() => {
     fs?.readFile(url, (error, contents = Buffer.from("")) => {
@@ -27,7 +32,15 @@ const MonacoEditor: React.FC<ComponentProps> = ({ id }) => {
         appendFileToTitle(basename(url));
       }
     });
-  }, [appendFileToTitle, fs, url]);
+
+    return () => {
+      if (editor) {
+        editor.getModel()?.dispose();
+        editor.dispose();
+        cleanUpGlobals(["monaco"]);
+      }
+    };
+  }, [appendFileToTitle, editor, fs, url]);
 
   loader.config({ paths: { vs: "/libs/monaco/vs" } });
 
@@ -37,7 +50,12 @@ const MonacoEditor: React.FC<ComponentProps> = ({ id }) => {
       className="w-full text-white"
       style={{ height: `calc(100% - ${sizes.taskbar.height})` }}
     >
-      <Editor path={url} theme="vs-dark" value={value} />
+      <Editor
+        path={url}
+        theme="vs-dark"
+        value={value}
+        onMount={(mountedEditor) => setEditor(mountedEditor)}
+      />
     </div>
   );
 };
