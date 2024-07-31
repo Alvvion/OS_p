b25a84b9ae0ba572a477abc272d59e61e6ae7daa
Utkarsh <alvvion@gmail.com>
2024-07-31 14:30:33 +0530

    Build Check

--- components/system/StartMenu/AppBox.tsx
+++ components/system/StartMenu/AppBox.tsx
@@ -3,7 +3,12 @@ import FileManager from "@/components/system/FileManager";
 const AppBox: React.FC = () => (
   <div className="mt-6 flex flex-col justify-start w-[90%]">
     <h3 className="text-white ml-4">All Apps</h3>
-    <FileManager url="/Users/Public/Start Menu" view="start" hideLoading />
+    <FileManager
+      url="/Users/Public/Start Menu"
+      view="start"
+      hideLoading
+      scrollable={false}
+    />
   </div>
 );
 
